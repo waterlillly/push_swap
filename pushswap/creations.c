3fa3f63a589d02f3cb8e5fa6df28d27621a6bad9
Hexelillyfee <131495080+waterlillly@users.noreply.github.com>
2024-03-18 16:44:32 +0000

    abc

--- pushswap/creations.c
+++ pushswap/creations.c
@@ -6,26 +6,21 @@
 /*   By: lbaumeis <lbaumeis@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 20:26:00 by lbaumeis          #+#    #+#             */
-/*   Updated: 2024/03/18 16:38:25 by lbaumeis         ###   ########.fr       */
+/*   Updated: 2024/03/18 16:44:15 by lbaumeis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 void	add_node(t_list **head, int value)
-
-void	add_node(t_list **head, int value)
 {
 	t_list	*node;
-	t_list	*temp;
 	t_list	*temp;
 
 	node = NULL;
 	temp = *head;
-	temp = *head;
 	node = (t_list *)malloc(sizeof(t_list));
 	if (!node)
-		return ;
 		return ;
 	node->data = value;
 	node->next = NULL;
@@ -42,40 +37,22 @@ void	add_node(t_list **head, int value)
 		temp->next = node;
 	}
 	node->next = NULL;
-	if (*head == NULL)
-	{
-		node->prev = NULL;
-		*head = node;
-	}
-	else
-	{
-		while (temp->next != 0)
-			temp = temp->next;
-		node->prev = temp;
-		temp->next = node;
-	}
 }
 
 t_list	**fill_stack(t_list **stack, char **input)
-t_list	**fill_stack(t_list **stack, char **input)
 {
-	t_list	**head;
 	t_list	**head;
 	t_list	*s;
 	int		x;
 	
 	head = stack;
-	head = stack;
 	s = *stack;
 	x = 1;
-	add_node(head, ft_atoi(input[x]));
 	add_node(head, ft_atoi(input[x]));
 	s = s->next;
 	x++;
 	while (input[x] && s && s->next)
-	while (**input && s && s->next)
 	{
-		add_node(stack, ft_atoi(input[x]));
 		add_node(stack, ft_atoi(input[x]));
 		s = s->next;
 		x++;
@@ -139,92 +116,14 @@ int	ft_arr_len(char **arr)
 	while (arr[x] != NULL)
 		x++;
 	return (x);
-	add_node(stack, 0);
-	return (head);
 }
 
-char	**one(char **av, char **input)
-{
-	input = ft_split(av[1], ' ');
-	if (!input)
-		return (msg(8), NULL);
-	if (input[1] == NULL)
-		return (msg(2), NULL);
-	else
-		return (input);
-}
-
-char	**two(int ac, char **av, char **input)
-{
-	int	x;
-	int	y;
-
-	x = 1;
-	y = 0;
-	input = (char **)malloc(sizeof(char *) * ac);
-	if (!input)
-		return (NULL);
-	while (av[x] && input[y] && x <= ac)
-	{
-		input[y] = av[x];
-		x++;
-		y++;
-	}
-	input[y] = NULL;
-	return (input);
-}
-
-char	**get_input(int ac, char **av)
-{
-	char	**input;
-
-	input = NULL;
-	if (ac == 2)
-	{
-		input = one(av, input);
-		if (!input)
-			return (0);
-	}
-	else
-	{
-		input = two(ac, av, input);
-		if (!input)
-			return (0);
-	}
-	return (input);
-}
-
-int	ft_arr_len(char **arr)
-{
-	int	x;
-
-	x = 0;
-	while (arr[x] != NULL)
-		x++;
-	return (x);
-}
-
-t_list	**stack(int	ac, char **av)
 t_list	**stack(int	ac, char **av)
 {
 	t_list	**stack;
 	char	**input;
 	int		len;
-	char	**input;
-	int		len;
 
-	stack = NULL;
-	input = NULL;
-	len = 0;
-	if (av == NULL)
-		len = ac;
-	else
-	{
-		input = get_input(ac, av);
-		if (!input)
-			return (NULL);
-		len = ft_arr_len(input) + 1;
-	}
 	input = NULL;
 	len = 0;
 	if (av == NULL)
@@ -238,13 +137,6 @@ t_list	**stack(int	ac, char **av)
 	}
 	stack = (t_list **)malloc(sizeof(t_list *) * len);
 	if (!stack)
-		return (NULL);
-	if (av != NULL)
-	{
-		stack = fill_stack(stack, input);
-		if (!stack)
-			return (msg(5), ft_free(stack), NULL);
-	}
 		return (NULL);
 	if (av != NULL)
 	{

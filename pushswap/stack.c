f484baa48b4319c2772ed9fc98407ec645046c13
Hexelillyfee <131495080+waterlillly@users.noreply.github.com>
2024-05-30 10:05:19 +0000

    ...

--- pushswap/stack.c
+++ pushswap/stack.c
@@ -6,7 +6,7 @@
 /*   By: lbaumeis <lbaumeis@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 15:14:40 by lbaumeis          #+#    #+#             */
-/*   Updated: 2024/05/30 09:52:55 by lbaumeis         ###   ########.fr       */
+/*   Updated: 2024/05/30 10:05:13 by lbaumeis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ void	fill_stack(int ac, char **av, t_list **head)
 	int		x;
 	int		y;
 
+	input = NULL;
 	x = 0;
 	if (ac > 2)
 	{
